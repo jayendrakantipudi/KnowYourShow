58dcf355f95e28882090166ea98e22093f1da8d3
Bhogadi Yashwanth <bhogadiyashwanth9@gmail.com>
2019-04-13 23:53:10 +0530

    Added search result page

--- KYS/KYS/urls.py
+++ KYS/KYS/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.search,name='search'),
+    path('',views.mainpage,name='mainpage'),
+    path('search',views.search,name='search'),
     path('',include('show.urls')),
     path('',include('cast.urls')),
 ]
